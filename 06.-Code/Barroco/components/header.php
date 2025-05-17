59733d57894b1327c626aadb3e54cba404201b00
ImaGaf <oswaldoj.tipan@gmail.com>
2025-05-16 22:59:56 -0500

    Structure done

---
+++ 06.-Code/Barroco/components/header.php
@@ -0,0 +1,35 @@
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="../assests/css/header.css">
+        <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap" rel="stylesheet">
+        <title>Barroco</title>
+    </head>
+
+    <body>
+        <header class="barroco-header">
+            <div class="header-container">
+                <img class="logo" src="../assests/imgs/barrocoLogo.png" alt="">
+                <h1>BARROCO</h1>
+                <div class="icon-links">
+                    <a href="#"><i class="fa-regular fa-user"></i></a>
+                    <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                    <a href="#"><i class="fa-solid fa-clipboard-list"></i></a>
+                </div>
+            </div>
+            <nav class="nav-links">
+                <a href="#">Jarros</a>
+                <a href="#">Platos</a>
+                <a href="#">Floreros</a>
+                <a href="#">Macetas</a>
+                <a href="#">Jarrones</a>
+                <a href="#">Cuencos y bowls</a>
+            </nav>
+        </header>
+    </body>
+
+    </html>
