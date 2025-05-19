1ef3a999f8c0f53bd7a9b17c450ba0ccf527400b
Rodrigo Fernando Sandoval Lara <132413789+rfsandoval1@users.noreply.github.com>
2025-05-18 23:54:15 -0500

    register

--- 06.-Code/Barroco/pages/register.php
+++ 06.-Code/Barroco/pages/register.php
@@ -7,33 +7,35 @@
 </head>
 <body>
     <div class="contenedor">
+        <div class="imagen"></div>
         <div class="formulario">
             <h2>CREAR CUENTA</h2>
-            <form action="register.php" method="post">
-                <label for="usuario">Usuario</label>
-                <input type="text" id="usuario" name="usuario" placeholder="Usuario" required>
 
-                <label for="email">Correo electrónico</label>
-                <input type="email" id="email" name="email" placeholder="ejemplo@correo.com" required>
+            <div class="form-content">
+                <form action="register.php" method="post">
+                    <label for="email">Correo electrónico</label>
+                    <input type="email" id="email" name="email" placeholder="ejemplo@correo.com" required>
 
-                <label for="contrasena">Contraseña</label>
-                <input type="password" id="contrasena" name="contrasena" placeholder="Contraseña" required>
+                    <label for="usuario">Usuario</label>
+                    <input type="text" id="usuario" name="usuario" placeholder="Usuario" required>                
 
-                <label for="confirmar">Confirmar contraseña</label>
-                <input type="password" id="confirmar" name="confirmar" placeholder="Repite la contraseña" required>
+                    <label for="contrasena">Contraseña</label>
+                    <input type="password" id="contrasena" name="contrasena" placeholder="Contraseña" required>
 
-                <button type="submit">REGISTRARSE</button>
-            </form>
+                    <label for="confirmar">Confirmar contraseña</label>
+                    <input type="password" id="confirmar" name="confirmar" placeholder="Repite la contraseña" required>
 
-            <div class="google-login">
-                <span>o</span>
-                <div class="google-icon">G</div>
+                    <button type="submit">REGISTRARSE</button>
+                </form>
+
+                <div class="google-login">
+                    <span>o</span>
+                    <div class="google-icon">G</div>
+                </div>
+
+                <p class="registro">¿Ya tienes una cuenta? <a href="login.php">Inicia sesión</a></p>
             </div>
-
-            <p class="registro">¿Ya tienes una cuenta? <a href="login.php">Inicia sesión</a></p>
         </div>
-        <div class="imagen"></div>
     </div>
 </body>
 </html>
-
