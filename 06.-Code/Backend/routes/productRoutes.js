2dffc203677b04ff31b6e5a5a0dad1d81f026fdd
imagaf <oswaldoj.tipan@gmail.com>
2025-06-27 06:40:39 -0500

    add functions

--- 06.-Code/Backend/routes/productRoutes.js
+++ 06.-Code/Backend/routes/productRoutes.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
+
+router.get("/productsAvailable", productController.getAvailable);
+router.get("/productsDiscounted", productController.getCustomDiscounted);
+router.post("/products/:idProduct/purchase", productController.purchase);
+
 router.get("/", productController.getAll);
 router.get("/:id", productController.getById);
 router.post("/", productController.create);
 router.put("/:id", productController.update);
 router.delete("/:id", productController.remove);
 
-router.get("/products-available", productController.getAvailable);
-router.get("/products-discounted", productController.getCustomDiscounted);
-router.post("/products/:idProduct/purchase", productController.purchase);
 
 module.exports = router;
