13dee1929a86f0b5f2417e4814839086438be37e
imagaf <oswaldoj.tipan@gmail.com>
2025-08-03 16:12:20 -0500

    customer model updated

--- 06.-Code/Backend/models/customer.js
+++ 06.-Code/Backend/models/customer.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
 
-const customerSchema = new mongoose.Schema({
-  phone: { type: String, required: true },
-  billingAddress: { type: String, required: true }
-},
-{
-    collection: "customers"
-});
+const customerSchema = new mongoose.Schema(
+  {
+    idCustomer: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: String, required: true },
+    billingAddress: { type: String, required: true },
+    shippingAddress: { type: String, required: false },
+  },
+  {
+    collection: "customers",
+    timestamps: true, 
+  }
+);
 
 const Customer = mongoose.model("Customer", customerSchema);
 
