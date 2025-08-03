40f6266c1543e7e6011e84fc38bdb1df188ac6cf
imagaf <oswaldoj.tipan@gmail.com>
2025-08-03 16:29:42 -0500

    update customer id

--- 06.-Code/Backend/models/customer.js
+++ 06.-Code/Backend/models/customer.js
@@ -1,22 +1,34 @@
 const mongoose = require("mongoose");
+const Counter = require("./counter");
 
 const customerSchema = new mongoose.Schema(
   {
-    idCustomer: { type: String, required: true, unique: true },
+    idCustomer: { type: Number, unique: true, index: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     phone: { type: String, required: true },
     billingAddress: { type: String, required: true },
-    shippingAddress: { type: String, required: false },
+    shippingAddress: { type: String },
   },
   {
     collection: "customers",
-    timestamps: true, 
+    timestamps: true,
   }
 );
 
-const Customer = mongoose.model("Customer", customerSchema);
+customerSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    const counter = await Counter.findByIdAndUpdate(
+      { _id: "customerId" },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+    );
+    this.idCustomer = counter.seq;
+  }
+  next();
+});
 
+const Customer = mongoose.model("Customer", customerSchema);
 module.exports = Customer;
