b3740c8014bdb2fe1efab039e4a0b2a2adda1a95
imagaf <oswaldoj.tipan@gmail.com>
2025-08-12 13:42:31 -0500

    secuence done

--- 06.-Code/Backend/app.js
+++ 06.-Code/Backend/app.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: '*', 
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
